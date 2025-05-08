04a7788ad2ccfed778cde97944300c820fef2e7c
Alexey Seleznev <rib@live.ru>
2025-05-08 13:17:26 +0300

    init

---
+++ src/main/java/ru/r1b/webrisetask/WebRiseTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.r1b.webrisetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRiseTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRiseTaskApplication.class, args);
+    }
+
+}
